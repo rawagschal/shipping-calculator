9005e305f7b1dac2a105da2866956525fe1f5d65
Rachel Wagschal <rawagschal@gmail.com>
2021-12-30 15:06:53 -0800

    bootstrapped

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ function calculate($zip, $weight) {
         return $weight * $weight - $weight + 1.25;
     } else if ($zip === 55556) {
         return $weight + .75;
-    }
+    } 
 }
 
 if (isset($_POST['zip']) && isset($_POST['weight'])) {
@@ -18,30 +18,36 @@ if (isset($_POST['zip']) && isset($_POST['weight'])) {
 <html>
 <head>
     <title>Shipping Calculator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Boostrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
+<div class='card'>
+    <h1 class='card-title'>Shipping Calculator</h1>
 
-    <h1>Shipping Calculator</h1>
-
-        
     <form action='' method='post' id='shipping-form'>
-        
-        <p> Zip Code 
-            <input type='text' name='zip' id='zip' required='required'/>
-        </p>
+        <div class='form-group'>
+            <label for='zip'>Zip Code</label>
+                <input type='text' class='form-control' name='zip' id='zip' required='required'/>
+            <label for='weight'>Weight (lbs)</label>
+                <input type='text' class='form-control' name='weight' id='weight' required='required'/>                
+            <button type='submit' class='btn btn-primary'>Calculate</button>
+        </div>
             
-        <p> Weight (lbs)
-            <input type='text' name='weight' id='weight' required='required'/>
-        </p>
-        
-        <p>
-            <input type='submit'/>
-        </p>
-        
         <?php if (isset($total)) { ?>
-            <h2>Total Shipping Cost: $<?php echo $total ?></h2>
-        <?php } ?>
+            <h2>Total Shipping Cost: $
+                <?php echo $total ?>
+            </h2>
+        <?php }  ?>
+</div>
 
-</body>
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    </body>
 </html>
