e4e9deaa024a4264d2797557ebf248e95f7345c8
Rachel Wagschal <rawagschal@gmail.com>
2022-01-04 17:50:24 -0800

    removed bootstrap

--- index.php
+++ index.php
@@ -20,14 +20,12 @@ if (isset($_POST['zip']) && isset($_POST['weight'])) {
     <title>Shipping Calculator</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Boostrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel='stylesheet' href='/index.css'>
 </head>
 <body>
 
-<div class='card'>
-    <h1 class='card-title'>Shipping Calculator</h1>
+<div class='parent'>
+    <h1 class='cta'>Shipping Calculator</h1>
+        <h3 class='cta'> Please enter the following:</h3>
 
     <form action='' method='post' id='shipping-form'>
         <div class='form-group'>
@@ -44,11 +42,5 @@ if (isset($_POST['zip']) && isset($_POST['weight'])) {
             </h2>
         <?php }  ?>
 </div>
-
-    <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    </body>
+</body>
 </html>
